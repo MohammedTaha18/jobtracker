90ec6704386991ee78f7948655168433f240a5ae
MohammedTaha18 <scorpiontheking1000@gmail.com>
2025-05-06 16:56:33 +0530

    some more

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/https://github.com/MohammedTaha18/jobtracker.git/",
+  base: '/jobtracker/'
 })
